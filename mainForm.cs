607b4fe7ab765fd8b376ea16fb4ce83a560bb223
INF181-Project <142520405+DieMalEen@users.noreply.github.com>
2024-10-29 17:58:44 +0200

    View
    Added some example students to student.txt. View button works.

--- mainForm.cs
+++ mainForm.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,11 +23,37 @@ namespace Group1_PRG282_Project
         {
             var addStudent = new addForm();
             addStudent.ShowDialog();
+
+
         }
 
         private void btnView_Click(object sender, EventArgs e)
         {
+            // Makes grid view visible
             dataStudents.Visible = true;
+
+            // Read data in student.txt
+            var rows = File.ReadAllLines(@"..\..\students.txt");
+
+            var list = (from column in rows.ToList()
+                        select new gridHeadings
+                        {
+                            ID = column.Split(new string[] {","}, StringSplitOptions.None)[0],
+                            Name = column.Split(new string[] {","}, StringSplitOptions.None)[1],
+                            Age = column.Split(new string[] {","}, StringSplitOptions.None)[2],
+                            Course = column.Split(new string[] {","}, StringSplitOptions.None)[3]
+                        }).ToList();
+
+            // Add data from text to grid
+            dataStudents.DataSource = list;
         }
+    }
+
+    public class gridHeadings
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string Age { get; set; }
+        public string Course { get; set; }
     }
 }
